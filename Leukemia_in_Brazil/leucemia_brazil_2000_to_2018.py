90f1e1717b5c5cb83f2e2c92dbdc4e5dba48283d
Murilo Moraes <66094955+Moraes97-Murilo@users.noreply.github.com>
2020-12-02 10:20:11 -0300

    Add files via upload

---
+++ Leukemia_in_Brazil/leucemia_brazil_2000_to_2018.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+data = open("Modelo01_20201202103026.csv").readlines()
+
+x = []
+y = []
+
+for i in range(len(data)):
+  if i >= 3:
+    line = data[i].split(";")
+    x.append(float(line[0]))
+    y.append(float(line[2]))
+plt.scatter(x,y, marker='+', color='k', s=100)
+plt.plot(x,y, color='r', linestyle='--')
+plt.bar(x,y, color='#A9CCE3')
+plt.title("Deaths by Leukemia in Brazil(2000-2018)")
+plt.xlabel("Year")
+plt.ylabel("Deaths by Leukemia")
+plt.show()
+#plt.savefig("Graph_Leukemia.pdf")
