79cb02182c6087bc6b6622eb2ad66d76a081937a
Murilo Moraes <66094955+Moraes97-Murilo@users.noreply.github.com>
2020-12-02 09:51:04 -0300

    Update leucemia_brazil_2000_to_2018.py

--- leucemia_brazil_2000_to_2018.py
+++ leucemia_brazil_2000_to_2018.py
@@ -1,5 +1,22 @@
-#
-#
+#data came from https://mortalidade.inca.gov.br/
+#the CSV file will be at the repo
 
 import matplotlib.pyplot as plt
 
+data = open("Modelo01_20201202103026.csv").readlines()
+
+x = []
+y = []
+
+for i in range(len(data)):
+  if i >= 3:
+    line = dados[i].split(";")
+    x.append(float(line[0]))
+    y.append(float(line[2]))
+plt.scatter(x,y, marker='s', color='k', s='250')
+plt.plot(x,y)
+plt.bar(x,y)
+plt.tittle("Deaths by Leukemia in Brazil(2000-2018)")
+plt.xlabel("Year")
+plt.ylabel("Deaths by Leukemia")
+plt.show()
